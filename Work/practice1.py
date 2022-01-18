0bf1723b60d8d2fa9cf1b9d0e4a7e9474e9603e9
Yonatan <89758766+YonaNeg@users.noreply.github.com>
2022-01-18 01:00:33 -0500

    upload personal practice file

---
+++ Work/practice1.py
@@ -0,0 +1,19 @@
+def sumcount(n):
+    '''
+    Returns the sume of the first n integers
+    '''
+    total = 0
+    while n > 0:
+        total += 1
+        n -= 1
+    return total
+
+import math
+x = math.sqrt(10)
+
+import urllib.request
+u = urllib.request.urlopen('http://www.python.org/')
+data = u.read()
+
+
+
